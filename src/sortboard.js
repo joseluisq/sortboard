7e8ba47235c665878724dbcfaceb1aa36a0a1d27
Jose Luis Quintana <joseluisquintana20@gmail.com>
2014-11-30 17:33:12 -0500

    First version 1.0.0.

---
+++ src/sortboard.js
@@ -0,0 +1,234 @@
+/*
+ * Sortboard v1.0.0
+ * Release: 30/11/2014
+ * Author: Jose Luis Quintana <joseluisquintana20@gmail.com>
+ * https://github.com/joseluisq/sortboardjs
+ * MIT Licence
+ */
+(function(window) {
+  'use strict';
+
+  if (window.Sortboard) {
+    return;
+  }
+
+  var Sortboard = function(element, options) {
+    this.currentFilter = 'all';
+    this.elements = [];
+    this.found = [];
+    this.notfound = [];
+
+    this.options = {
+      gutter: 0,
+      hiddenClass: '.hidden',
+      itemsMatchName: 'li',
+      filterComplete: null,
+      sortComplete: null
+    };
+
+    this.addEvents = function() {
+      var that = this;
+
+      window.addEventListener('resize', function() {
+        that.sort();
+      }, false);
+
+      that.sort();
+    };
+
+    this.setOptions = function(options) {
+      this.options = this.merge(this.options, options);
+    };
+
+    this.merge = function(a, b) {
+      var i;
+
+      if (b) {
+        for (i in b) {
+          a[i] = b[i];
+        }
+      }
+
+      return a;
+    };
+
+    this.init = function(element, options) {
+      if (typeof element === 'object') {
+        if (element.nodeName) {
+          this.setElement(element);
+          this.setOptions(options);
+          this.addEvents();
+        } else {
+          this.setOptions(options);
+        }
+      } else {
+        this.error('Sortboard requires an HTML element.');
+      }
+    };
+
+    this.setElement = function(element) {
+      this.element = element;
+    };
+
+    this.sort = function() {
+      var item, gutter, itemList,
+        i = 0,
+        n = 0,
+        w = 0,
+        h = 0,
+        sw = 0,
+        tw = 0,
+        th = 0,
+        rtw = 0,
+        cth = 0,
+        ctw = 0;
+
+      sw = this.element.parentElement.offsetWidth;
+      gutter = this.options.gutter;
+      itemList = this.getElements(this.options.itemsMatchName);
+      this.size = itemList.length;
+      var regx = new RegExp('(' + this.options.hiddenClass.replace('.', '') + ')+', 'i');
+
+      for (i = 0; i < this.size; ++i) {
+        item = itemList[i];
+
+        if (!item.className.match(regx)) {
+          w = item.offsetWidth;
+          h = item.offsetHeight;
+
+          if (tw >= (sw - h - gutter)) {
+            tw = 0;
+            th += h + gutter;
+
+            if (!ctw) {
+              ctw = n * w + ((n * gutter) - gutter);
+            }
+          } else {
+            tw += n ? gutter : 0;
+          }
+
+          tw += w;
+          rtw = tw - w;
+          cth = th + h;
+          n++;
+
+          item.setAttribute('data-cords', rtw + ',' + th);
+          this.translate(item, rtw + ',' + th, false);
+        }
+      }
+
+      this.element.style.width = (ctw ? ctw : (n * w) + (((n < 2 ? n : n - 1)) * gutter)).toString() + 'px';
+      this.element.style.height = cth.toString() + 'px';
+
+      if (this.options.sortComplete && typeof this.options.sortComplete === 'function') {
+        this.options.sortComplete();
+      }
+    };
+
+    this.filterBy = function(filter) {
+      if (this.currentFilter !== filter) {
+        var i, attr, match, cords, regx, item, itemList, matches = [];
+
+        regx = new RegExp(filter, 'i');
+        itemList = this.getElements(this.options.itemsMatchName);
+
+        for (i = 0; i < this.size; ++i) {
+          item = itemList[i];
+          attr = item.getAttribute('data-filter');
+
+          if (attr && attr.search(regx) !== -1) {
+            matches.push(item);
+          }
+        }
+
+        if (!matches.length && filter !== 'all') {
+          return;
+        }
+
+        this.currentFilter = filter;
+        this.found = [];
+        this.notfound = [];
+
+        for (i = 0; i < this.size; ++i) {
+          item = itemList[i];
+          cords = item.getAttribute('data-cords');
+
+          if (filter === 'all') {
+            this.found.push(item);
+            this.translate(item, cords, false);
+          } else {
+            attr = item.getAttribute('data-filter');
+            match = (attr && attr.search(regx) !== -1);
+
+            if (match) {
+              this.found.push(item);
+            } else {
+              this.notfound.push(item);
+            }
+
+            this.translate(item, cords, !match);
+          }
+        }
+
+        if (this.options.filterComplete && typeof this.options.filterComplete === 'function') {
+          this.options.filterComplete({
+            found: this.found || [],
+            notfound: this.notfound || []
+          });
+        }
+
+        this.sort();
+      }
+    };
+
+    this.translate = function(item, cords, hide) {
+      var matrix = 'matrix(1,0,0,1,' + cords + ') scale(' + (hide ? '0.001' : '1') + ')';
+      item.className = hide ? this.options.hiddenClass.replace('.', '') : '';
+      item.style.setProperty('opacity', hide ? '0' : '1');
+      item.style.setProperty('-webkit-transform', matrix);
+      item.style.setProperty('-moz-transform', matrix);
+      item.style.setProperty('transform', matrix);
+    };
+
+    this.getElements = function(childrenName) {
+      if (!this.elements.length) {
+        this.elements = childrenName.match(/^\.(.+)$/) ? this.element.getElementsByClassName(childrenName) : this.element.getElementsByTagName(childrenName);
+      }
+
+      return this.elements;
+    };
+
+    this.error = function(str) {
+      if (window.console) {
+        window.console.error(str);
+      }
+    };
+
+    this.init(element, options);
+  };
+
+  window.Sortboard = function(element, options) {
+    var sb = new Sortboard(element, options);
+
+    return {
+      filterBy: function(filter) {
+        sb.filterBy(filter);
+      },
+      sort: function() {
+        sb.sort();
+      },
+      getFilter: function() {
+        return sb.currentFilter;
+      },
+      getItems: function() {
+        return sb.elements;
+      },
+      getFoundItems: function() {
+        return sb.found;
+      },
+      getNotFoundItems: function() {
+        return sb.notfound;
+      }
+    };
+  };
+})(this);
---
+++ test/test.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var fs = require('fs');
+var jsdom = require('mocha-jsdom');
+var expect = require('chai').expect;
+
+describe('Sortboard', function() {
+  var Sortboard;
+  var sortlist;
+
+  jsdom({
+    src: fs.readFileSync('src/sortboard.js', 'utf-8')
+  });
+
+  before(function() {
+    Sortboard = global.window.Sortboard;
+    document.body.innerHTML = fs.readFileSync('test/test.html', 'utf-8');
+    sortlist = document.getElementById('sortlist');
+  });
+
+  it('class should be initialized successfully.', function() {
+    var sortlist = document.getElementById('sortlist');
+    var sb = new Sortboard(sortlist);
+
+    expect(sb).to.be.an('object');
+  });
+
+  describe('.sortComplete()', function() {
+
+    it('callback should be called successfully.', function(done) {
+      var sb = new Sortboard(sortlist, {
+        sortComplete: done
+      });
+    });
+
+  });
+
+  describe('.filterComplete()', function() {
+
+    it('callback should be called successfully.', function(done) {
+      var sb = new Sortboard(sortlist, {
+        filterComplete: function() {
+          done();
+        }
+      });
+
+      sb.filterBy('royalty');
+    });
+  });
+
+  describe('.filterComplete(data)', function() {
+    it('param value `data` should have `found` and `notfound` properties.', function(done) {
+      var sb = new Sortboard(sortlist, {
+        filterComplete: function(data) {
+          expect(data).to.have.property('found');
+          expect(data).to.have.property('notfound');
+          done();
+        }
+      });
+
+      sb.filterBy('first-line');
+    });
+
+    it('`found` and `notfound` properties values should be an `Array` object.', function(done) {
+      var sb = new Sortboard(sortlist, {
+        filterComplete: function(data) {
+          expect(data).to.have.property('found').that.is.an('array');
+          expect(data).to.have.property('notfound').that.is.an('array');
+          done();
+        }
+      });
+
+      sb.filterBy('infantry');
+    });
+  });
+
+});
